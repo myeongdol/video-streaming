277031c5bffffa93aee7574b57cd19f10a9c67ce
MyeongHyun <myhyun729@naver.com>
2020-03-15 16:17:36 +0900

    init project

---
+++ src/test/java/com/toy/videostreaming/VideoStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.videostreaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoStreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
