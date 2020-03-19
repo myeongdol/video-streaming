a58fabee7b31779eedda9f3ee455744f8a440c22
suheepark <sue.parksuhee@gmail.com>
2020-03-19 18:10:21 +0900

    SH:JSP 적용 설정 추가(오류해결)

--- src/main/java/com/toy/videostreaming/VideoStreamingApplication.java
+++ src/main/java/com/toy/videostreaming/VideoStreamingApplication.java
@@ -3,6 +3,7 @@ package com.toy.videostreaming;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +15,9 @@ public class VideoStreamingApplication {
 		SpringApplication.run(VideoStreamingApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+	@GetMapping("/")
+	public String index(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s", name);
-	}  
+	}
+
 }
