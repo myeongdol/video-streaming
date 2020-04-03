b9fe5213961463b4c9597b2f4b9a7a94a75cc407
suheepark <sue.parksuhee@gmail.com>
2020-04-03 17:15:14 +0900

    SH: 동영상 썸네일 생성 및 리스트 출력

--- src/main/java/com/toy/videostreaming/controller/BoardController.java
+++ src/main/java/com/toy/videostreaming/controller/BoardController.java
@@ -5,6 +5,10 @@ import com.toy.videostreaming.domain.Member;
 import com.toy.videostreaming.domain.Video;
 import com.toy.videostreaming.service.BoardService;
 import com.toy.videostreaming.service.VideoService;
+import org.jcodec.api.FrameGrab;
+import org.jcodec.api.JCodecException;
+import org.jcodec.common.model.Picture;
+import org.jcodec.scale.AWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,15 +18,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.imageio.ImageIO;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -49,14 +54,14 @@ public class BoardController {
     public String readFile(@PathVariable int vno, HttpServletResponse response) throws IOException {
         if(vno==0) { return null; }
 
-        // 첨부파일 정보
+        // 썸네일 가져오기
         Video video = videoService.getOne(vno);
 
         response.setContentType( "image/gif" );
         ServletOutputStream bout = response.getOutputStream();
-        String attachFile = SAVE_PATH + video.getFilePath() + "/" + video.getFileName();
+        String thumbnail = SAVE_PATH + "img/" + video.getFilePath() + "/" + video.getFileName();
 
-        FileInputStream file = new FileInputStream(attachFile);
+        FileInputStream file = new FileInputStream(thumbnail);
         int length;
         byte[] buffer = new byte[10];
         while ( ( length = file.read( buffer ) ) != -1 )
@@ -101,7 +106,7 @@ public class BoardController {
         return view;
     }
 
-    // 첨부파일
+    // 첨부파일 처리
     public int uploadVideo(MultipartFile file, String memId) {
 
         SimpleDateFormat ymd = new SimpleDateFormat("yyyyMMdd");
@@ -110,17 +115,21 @@ public class BoardController {
         String newFileName = (String) hms.format(new Date()) + "_" + memId;
 
         String originFile = file.getOriginalFilename();
-        System.out.print(originFile);
         String originFileName = originFile.split("\\.")[0];
         String extension = originFile.split("\\.")[1];
 
-        // 파일 업로드 처리
         try {
-            if(!writeFile(file, newFilePath, newFileName)) {
+            // 동영상 저장
+            if(!saveFile(file, makeDir("video",newFilePath), newFileName)) {
+                return 0;
+            }
+
+            // 썸네일 저장
+            if(!saveThumbnail(file, makeDir("img",newFilePath), newFileName)) {
                 return 0;
             }
         }
-        catch (IOException e) {
+        catch (IOException | JCodecException e) {
             throw new RuntimeException(e);
         }
 
@@ -134,22 +143,51 @@ public class BoardController {
         return videoService.add(video);
     }
 
-    // 파일 저장
-    private boolean writeFile(MultipartFile multipartFile, String saveFilePath, String saveFileName)
-            throws IOException {
-
-        // 폴더 확인
-        File directory = new File(SAVE_PATH + saveFilePath);
+    // 파일 저장경로
+    private String makeDir(String type, String saveFilePath) {
+        File directory = new File(SAVE_PATH + type + "/" + saveFilePath);
 
         if(!directory.exists()) {
             directory.mkdirs();
         }
 
+        return directory.toString();
+    }
+
+    // 동영상 저장
+    private boolean saveFile(MultipartFile multipartFile, String saveFilePath, String saveFileName)
+            throws IOException {
+
         byte[] data = multipartFile.getBytes();
-        FileOutputStream fos = new FileOutputStream(SAVE_PATH + saveFilePath + "/" + saveFileName);
+        FileOutputStream fos = new FileOutputStream(saveFilePath + "/" + saveFileName);
         fos.write(data);
         fos.close();
 
         return true;
+    }
+
+    // 썸네일 저장
+    public boolean saveThumbnail(MultipartFile source, String saveFilePath, String saveFileName)
+            throws IOException, JCodecException {
+
+        // 파일 변환
+        File srcFile = new File(saveFilePath.replace("img","video")+"/"+source.getOriginalFilename());
+        source.transferTo(srcFile);
+
+        File thumbnail = new File(saveFilePath+"/"+saveFileName);
+
+        Picture picture = FrameGrab.getNativeFrame(srcFile, 0);
+
+        BufferedImage bufferedImage = AWTUtil.toBufferedImage(picture);
+        ImageIO.write(bufferedImage, "png", thumbnail);
+
+        if(thumbnail.exists() && thumbnail.length()>1) {
+            // 임시 생성 파일 삭제
+            srcFile.delete();
+
+            return true;
+        }
+
+        return false;
     }
 }
